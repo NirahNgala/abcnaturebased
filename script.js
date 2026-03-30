34a0be4e68f9612d13b57e1208ff5e2aac219f17
Nirah Ngala <faithngala02@gmail.com>
2026-03-30 10:22:42 +0300

    map

--- script.js
+++ script.js
@@ -44,3 +44,55 @@ slider.style.left = percent + "%";
 });
 
 });
+
+// INIT MAP
+var map = L.map('map', { zoomControl: false }).setView([-3.6, 39.8], 9);
+
+// TILE
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png').addTo(map);
+
+// MARKERS
+var markerStyle = {
+  radius: 7,
+  fillColor: "#5f9c43",
+  color: "#fff",
+  weight: 2,
+  fillOpacity: 1
+};
+
+var markers = [
+  L.circleMarker([-3.94, 39.74], markerStyle).addTo(map)
+    .bindPopup("<b>Mtwapa</b><br>Propagation Hub"),
+
+  L.circleMarker([-3.27, 39.95], markerStyle).addTo(map)
+    .bindPopup("<b>Kakuyuni</b><br>Experimental Farm"),
+
+  L.circleMarker([-2.8, 39.7], markerStyle).addTo(map)
+    .bindPopup("<b>Koromi Farm</b><br>Agroforestry Demonstration")
+];
+
+// SCROLL INTERACTION
+var steps = document.querySelectorAll(".step");
+
+function activateStep(step) {
+  var lat = step.dataset.lat;
+  var lng = step.dataset.lng;
+  var zoom = step.dataset.zoom;
+
+  map.flyTo([lat, lng], zoom, {
+    duration: 1.5
+  });
+}
+
+// OBSERVER
+var observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      activateStep(entry.target);
+    }
+  });
+}, {
+  threshold: 0.6
+});
+
+steps.forEach(step => observer.observe(step));
