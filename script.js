3c6cce6cb282fb6decab0ef25aced65fa5dbe0c4
Nirah Ngala <faithngala02@gmail.com>
2026-04-15 16:08:26 +0300

    hyy

--- script.js
+++ script.js
@@ -16,34 +16,7 @@ const hamburger = document.getElementById('hamburger');
     }
   });
 
-const sliders = document.querySelectorAll('.before-after');
-
-sliders.forEach(container => {
-
-const slider = container.querySelector('.slider');
-const afterImg = container.querySelector('.after');
-
-let isDragging = false;
-
-slider.addEventListener('mousedown', () => isDragging = true);
-window.addEventListener('mouseup', () => isDragging = false);
-
-window.addEventListener('mousemove', (e) => {
-if (!isDragging) return;
-
-const rect = container.getBoundingClientRect();
-let x = e.clientX - rect.left;
-
-if (x < 0) x = 0;
-if (x > rect.width) x = rect.width;
-
-const percent = (x / rect.width) * 100;
-
-afterImg.style.clipPath = `inset(0 ${100 - percent}% 0 0)`;
-slider.style.left = percent + "%";
-});
-
-});
+counters.forEach(counter => observer.observe(counter));
 // INIT MAP
 var map = L.map('map', { zoomControl: false }).setView([-3.6, 39.8], 9);
 
